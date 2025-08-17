1c1251cfc859b6e6de2778dfa1c21cedc253ead5
Rhys Miller <rjmills87@aol.com>
2025-08-17 19:48:05 +0100

    feat: add team-user relationship and team model attributes

--- app/Models/Team.php
+++ app/Models/Team.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Team extends Model
 {
-    //
+    protected $fillable = [
+        'name',
+        'age_group',
+    ];
+
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
